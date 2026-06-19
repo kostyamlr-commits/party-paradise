6667fa571d15d7c4362ed827ed29db7c6e270b5f
kostyamlr-commits <kostyamlr@gmail.com>
2026-06-19 20:38:12 +0300

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { domains: ['ae01.alicdn.com','ae02.alicdn.com','ae03.alicdn.com','ae04.alicdn.com'] },
+}
+module.exports = nextConfig
