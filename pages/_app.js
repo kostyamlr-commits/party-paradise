34e6403b4a691c24ef097466239a55fd1a7e3ec1
kostyamlr-commits <kostyamlr@gmail.com>
2026-06-20 07:16:08 +0300

    feat: mount BackToTop globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/globals.css'
+import BackToTop from '../components/BackToTop'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <BackToTop />
+    </>
+  )
 }
